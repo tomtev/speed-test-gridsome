fa8ea6493339e546e81c7aaf687599274416b4f1
Tommy Vedvik <tommyvedvik@gmail.com>
2019-10-02 13:21:08 +0200

    test

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1>About me</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto. asdf</p>
   </Layout>
 </template>
 
